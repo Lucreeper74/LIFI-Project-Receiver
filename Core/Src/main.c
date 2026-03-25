62324de849509dc0f27826cd9b89d419c746b485
Lucreeper74 <lucreeper74@gmail.com>
2026-03-26 00:05:50 +0100

    Added UART communication with the BLE module

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,7 +21,8 @@
 #include "adc.h"
 #include "dma.h"
 #include "i2c.h"
-#include "ssd1306_oled.h"
+#include "stm32f3xx_hal.h"
+#include "stm32f3xx_hal_uart.h"
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
@@ -30,6 +31,7 @@
 /* USER CODE BEGIN Includes */
 #include "stm32_opal_receiver.h"
 #include "stm32_opal_uart_rx_cmd.h"
+#include "ssd1306_oled.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -63,21 +65,22 @@ void SystemClock_Config(void);
 /* USER CODE BEGIN 0 */
 void display_summary(char* status_str, int bit_errors_count, int bit_valid_count) {
   char status_dis_str[20];
-  sprintf(status_dis_str, "Status = %s", status_str);
+  sprintf(status_dis_str, "R= %s", status_str);
 
   char ber_str[20];
-  sprintf(ber_str, "Bit Error = %d", bit_errors_count);
+  sprintf(ber_str, "Bit Error= %d", bit_errors_count);
 
   char bit_valid_str[24];
-  sprintf(bit_valid_str, "Bit Valid = %d", bit_valid_count);
+  sprintf(bit_valid_str, "Bit Valid= %d", bit_valid_count);
 
   ssd1306_clear_screen();
 
   ssd1306_draw_string(display_TAG, (TPosition){20, 0});
-  ssd1306_draw_string("Received Data Summary:", (TPosition){0, 8});
-  ssd1306_draw_string(status_dis_str, (TPosition){0, 16});
-  ssd1306_draw_string(ber_str, (TPosition){0, 2});
-  ssd1306_draw_string(bit_valid_str, (TPosition){0, 24});
+  ssd1306_draw_string("---Summary---", (TPosition){18, 8});
+
+  ssd1306_draw_string(status_dis_str, (TPosition){0, 24});
+  ssd1306_draw_string(ber_str, (TPosition){0, 32});
+  ssd1306_draw_string(bit_valid_str, (TPosition){0, 40});
 
   ssd1306_update();
 }
@@ -117,6 +120,7 @@ int main(void)
   MX_ADC1_Init();
   MX_TIM2_Init();
   MX_I2C1_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
 
   OPAL_Receiver_Init(&hadc1, &htim2);
@@ -161,16 +165,19 @@ int main(void)
 
         int bit_errors_count = 0;
         char* status_str = "UNSPECIFIED";
+        uint8_t status_int = 0x0;
 
         switch (decoding_status) {
           case OPAL_SUCCESS:
             HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
             status_str = "SUCCESS";
+            status_int = 0x1;
             bit_errors_count = 0;
             break;
             
           case OPAL_ERROR_CRC_MISMATCH:
             status_str = "CRC_MISMATCH";
+            status_int = 0x2;
             bit_errors_count = OPAL_Frame_getHammingDistance(&receivedFrame, &OPAL_TestFrame);
             break;
 
@@ -190,6 +197,38 @@ int main(void)
         
         // Display received data summary
         display_summary(status_str, bit_errors_count, bit_valid_count);
+
+        //
+
+        // Send summary to BLE module
+        // Send BER to BLE module
+        char ber_res[20];
+        strcpy(ber_res, "BER=");
+        char ber_str[12];
+        sprintf(ber_str, "%d\0", (uint16_t) ((bit_errors_count * 1000) / (bit_errors_count + bit_valid_count)));
+        strcat(ber_res, ber_str);
+        HAL_UART_Transmit(&huart1, (uint8_t*) &ber_res, sizeof(ber_res), 1);
+        HAL_Delay(100);
+
+        // Send pw level to BLE module
+        char pwr_res[20];
+        strcpy(pwr_res, "RX_PWR=");
+        char pwr_str[12];
+        sprintf(pwr_str, "%d\0", 50);
+        strcat(pwr_res, pwr_str);
+        HAL_UART_Transmit(&huart1, (uint8_t*) &pwr_res, sizeof(pwr_res), 1);
+        HAL_Delay(100);
+
+        // Send status to BLE module
+        char stat_res[20];
+        strcpy(stat_res, "STATUS=");
+        char stat_str[12];
+        sprintf(stat_str, "%d\0", status_int);
+        strcat(stat_res, stat_str);
+        HAL_UART_Transmit(&huart1, (uint8_t*) &stat_res, sizeof(stat_res), 1);
+        HAL_Delay(100);
+
+        //
 
         OPAL_Receiver_Start_Sniffing(&hrx); // Return to sniffing mode
     }
@@ -239,8 +278,10 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2|RCC_PERIPHCLK_I2C1
-                              |RCC_PERIPHCLK_ADC12|RCC_PERIPHCLK_TIM2;
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1|RCC_PERIPHCLK_USART2
+                              |RCC_PERIPHCLK_I2C1|RCC_PERIPHCLK_ADC12
+                              |RCC_PERIPHCLK_TIM2;
+  PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK2;
   PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
   PeriphClkInit.Adc12ClockSelection = RCC_ADC12PLLCLK_DIV1;
   PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_HSI;
